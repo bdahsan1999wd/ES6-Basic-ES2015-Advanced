a1d82118bccb80411b6620dad0ac1ae9a29ea48e
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-24 01:26:04 +0600

    more summary added

---
+++ 1.2-summary.js
@@ -0,0 +1,50 @@
+/* --------------Must Know : -------------- */
+/* 
+    1. var, let, const -----> !!!
+
+    2. template string -----> !!!
+        2.1: create multi-line string(``)
+        2.2: add dynamic variable (expression): `${}`
+
+    3. Arrow function -----> !!!
+        3.1: single parameter arrow function
+        3.2: two parameters arrow function
+        3.3: arrow function with function body.
+        and don't forget you have to return explicity
+
+    4. map, filter, find -----> !!!
+
+    5. optional chaining -----> !!!
+*/
+
+
+
+/* ---------------Good to have : --------------- */
+/* 
+    1. spread operator: [...numbers, 1, 2, 3, 44, 55, 3,] -----> !!!
+
+    2. destructuring -----> !!!
+        2.1: object destructuring. how to get property name as variable
+            const {x} = {x: 3, y:5}
+        2.2: array destructuring
+            const [a, b] = [3, 4, 55, 6, 77]
+
+    3. default parameter -----> !!!
+
+    4. forEach -----> !!!
+
+    5. dot notation vs bracket notation -----> !!!
+*/
+
+
+
+/* ---------------Should know the name : --------------- */
+/* 
+    1. reduce -----> !!!
+    
+    2. inheritance, super -----> !!!
+
+    3. (must) prototypical Inheritance chain -----> !!!
+
+    4. class, object -----> !!!
+*/
